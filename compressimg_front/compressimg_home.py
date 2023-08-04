69110534b53cd0059446ff1d8a7983403b67b4f2
LudoTreb <l.trebalag@gmail.com>
2023-08-04 19:37:16 +0200

    Create compressimg_home.py file. Just commented lines to remain how to build the home page.

---
+++ compressimg_front/compressimg_home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# titre du site
+st.title("compressIMG")
+
+# descriptif du site
+
+# h1 Step 1
+# title
+# widget drag & drop
+
+
+# h1 Step 2
+# title
+# 3 colones
+# - Name avec widget : st.text_input("You can rename your image compress", "name_file.png")
+# - Quality & resize avec widget : st.slider('Resize', 0, 100, (55))
+# - Extension avec widget : st.selectbox("your extension", ("PNG", "JPG", "WEBP"),)
+
+# h1 Step 3
+# title
+# 3 colones
+# - Compress avec widget : st.button("Compress")
+# - Une fois la compression terminée, le bouton Download apparait  avec widget : st.button("Download")
+# - Une fois le download terminé, un emoji pouce apparait.
