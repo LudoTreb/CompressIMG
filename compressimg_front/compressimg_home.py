f28185bc193a90a2b193fc82227ab06bee309a81
LudoTreb <l.trebalag@gmail.com>
2023-08-07 01:49:22 +0200

    create directory tools with functions to manipulate an image. Import these functions into compressimg.py

--- compressimg_front/compressimg_home.py
+++ compressimg_front/compressimg_home.py
@@ -1,15 +1,18 @@
 import streamlit as st
+from tools.tools_img import manipulate_img
+
 
 # titre du site
 st.title("compressIMG")
 
 # descriptif du site
 st.write(
-    "Descriptif du site : Lorem ipsum dolor sit amet consectetur. Metus lorem pulvinar quis urna bibendum gravida blandit non sed. Orci dui arcu tortor tortor imperdiet tortor hendrerit scelerisque."
+    """Descriptif du site : Lorem ipsum dolor sit amet consectetur.
+    Metus lorem pulvinar quis urna bibendum gravida blandit non sed.
+    Orci dui arcu tortor tortor imperdiet tortor hendrerit scelerisque."""
 )
 
 # h1 Step 1
-# title
 st.write("# 📂 Step 1")
 
 # widget drag & drop
@@ -20,7 +23,6 @@ uploaded_file = st.file_uploader(
 st.write("---")
 
 # h1 Step 2
-# title
 st.write("# 🛠️ Step 2")
 st.write("Custom your export file")
 
@@ -29,14 +31,14 @@ col_1, col_2, col_3 = st.columns(3)
 # - Name avec widget : st.text_input("You can rename your image compress", "name_file.png")
 with col_1:
     st.write("### Name")
-    st.text_input("You can rename your image compress", "name_file")
+    name_img = st.text_input("You can rename your image compress", "name_file")
 
 # - Quality & resize avec widget : st.slider('Resize', 0, 100, (55))
 with col_2:
     st.write("### Quality & Resize")
 
-    st.slider('Resize', 0, 100, (55))
-    st.select_slider(
+    resize_factor = st.slider('Resize', 0, 100, (55))
+    quality_img = st.select_slider(
         "Quality of your image",
         options=["Poor", "Low", "Medium", "Good", "High"],
     )
@@ -44,15 +46,22 @@ with col_2:
 # - Extension avec widget : st.selectbox("your extension", ("PNG", "JPG", "WEBP"),)
 with col_3:
     st.write("### Extension")
-    st.selectbox(
+    extention_img = st.selectbox(
         "Your extension",
-        ("PNG", "JPG", "WEBP"),
+        ("PNG", "JPEG", "WEBP"),
     )
 
 st.write("---")
 
+user_data_input = {
+    "img_converted_name": name_img,
+    "img_converted_extension": extention_img,
+    "img_converted_scale": resize_factor,
+    "img_converted_quality": quality_img,
+}
+
+
 # h1 Step 3
-# title
 st.write("# 🗜️ Step 3")
 st.write("Compress & download your compressed image ")
 # 3 colones
@@ -61,6 +70,10 @@ col_4, col_5, col_6 = st.columns(3)
 # - Compress avec widget : st.button("Compress")
 with col_4:
     compress_btn = st.button("Compress", use_container_width=True)
+    if uploaded_file:
+        if compress_btn:
+            img_compressed = manipulate_img(uploaded_file, user_data_input)
+            st.image(img_compressed)
 
 # - Une fois la compression terminée, le bouton Download apparait  avec widget : st.download_button()
 
---
+++ compressimg_front/tools/recherche.py
@@ -0,0 +1,149 @@
+from PIL import Image
+
+
+mon_image = "flower.png"
+user_data_input = {
+    "img_converted_name": "flower_compress",
+    "img_converted_extension": "JPEG",
+    "img_converted_scale": 50,
+    "img_converted_quality": "Medium",
+}
+
+# essai avec class
+# class ManipulateImage(Image.Image):
+#     def __init__(self, img_file):
+#         self.img = Image.open(img_file)
+
+#     def display_data(self):
+#         print(f"Data : dimension:{self.img.size}, format:{self.img.format}")
+
+#     def scale_img(self, data_input):
+#         scale_factor = data_input["img_converted_scale"] / 100
+
+#         new_img_width = int(self.img.width * scale_factor)
+#         new_img_height = int(self.img.height * scale_factor)
+
+#         resized_img = self.img.resize(
+#             (new_img_width, new_img_height),
+#         )
+
+#         self.img = resized_img
+
+
+# img = ManipulateImage(mon_image)
+
+# img.display_data()
+
+
+# img.scale_img(user_data_input)
+# img.display_data()
+
+mon_image = "flower.png"
+user_data_input = {
+    "img_converted_name": "flower_compress",
+    "img_converted_extension": "JPEG",
+    "img_converted_scale": 50,
+    "img_converted_quality": "Medium",
+}
+
+
+def formated_name_img(data_input):
+    """format the complete name of the img file.
+    Concatenation of name + extension.
+
+    Args:
+        data_input (dict): all data given by the user
+
+    Returns:
+        str: name file + extension
+    """
+
+    name_img = data_input["img_converted_name"].lower()
+
+    if data_input["img_converted_extension"] == "JPEG":
+        extention = "jpg"
+
+    extension_img = extention
+    ok_name_img = f"{name_img}.{extension_img}"
+
+    return ok_name_img
+
+
+def compress_img(img, data_input):
+    """Save an image with some parameter
+    like quality to reduce the weight.
+
+    Args:
+        img (class 'PIL.Image.Image'): image file
+        data_input (dict): all data given by the user
+
+    Returns:
+        class 'PIL.Image.Image': an compressed image
+    """
+    ok_name_img = formated_name_img(data_input)
+
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+
+    quality_correspondence = {
+        "Poor": 10,
+        "Low": 30,
+        "Medium": 50,
+        "Good": 70,
+        "High": 90,
+    }
+    quality = quality_correspondence.get(data_input["img_converted_quality"])
+
+    img_compressed = img.copy()
+    img_compressed.save(
+        ok_name_img,
+        format=data_input["img_converted_extension"],
+        optimize=True,
+        quality=quality,
+    )
+
+    return img_compressed
+
+
+def scale_img(img, data_input):
+    """Resize an image. Homothetique.
+
+    Args:
+        img (class 'PIL.Image.Image'): image file
+        data_input (dict): all data given by the user
+
+    Returns:
+         class 'PIL.Image.Image': an resized image
+    """
+    scale_factor = data_input["img_converted_scale"] / 100
+
+    new_img_width = int(img.width * scale_factor)
+    new_img_height = int(img.height * scale_factor)
+    resized_img = img.resize((new_img_width, new_img_height))
+
+    return resized_img
+
+
+def manipulate_img(img_path, data_input):
+    """The function use all the other to compress an image.
+    Reduce the quality, resize, rename, convert & save.
+
+    Args:
+        img_path (str): image file
+        data_input (dict): all data given by the user
+
+    Returns:
+        class 'PIL.Image.Image': a compressed image
+    """
+
+    img_uploaded = Image.open(img_path)
+
+    resized_img = scale_img(img_uploaded, data_input)
+    img_compressed = compress_img(resized_img, data_input)
+
+    return img_compressed
+
+
+img_compressed = manipulate_img(mon_image, user_data_input)
+print(type(img_compressed))
+print(img_compressed.size, img_compressed.format)
